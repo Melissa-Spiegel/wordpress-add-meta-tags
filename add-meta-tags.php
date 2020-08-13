a8d5fb2e0cfde1c79185d1d006b7fdc68cb0a275
mspiegel <mspiegel@indeed.com>
2020-08-13 19:09:01 -0400

    just having fun

--- add-meta-tags.php
+++ add-meta-tags.php
@@ -6,7 +6,7 @@
  * Version:           1.10.3000
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
+ * Author:            John Indeed
  * Author URI:        https://author.example.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
