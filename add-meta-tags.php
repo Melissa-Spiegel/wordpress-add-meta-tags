2c75423c3a1e33ff23d11c9c69f64c92dd8b500c
Melissa <mrspiegel@me.com>
2020-08-14 07:08:02 -0400

    updated version

--- add-meta-tags.php
+++ add-meta-tags.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Add Meta Tags
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           7.17.0
+ * Version:           1.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            John Indeed
